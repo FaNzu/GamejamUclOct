9f28f807cb63175a87798bf61b41352d25fee9c8
FaNzu <ask.hylleberg@gmail.com>
2023-10-11 09:55:49 +0200

    Setup visual studio

---
+++ GamejamCheese/Program.cs
@@ -0,0 +1,12 @@
+﻿using Spectre.Console;
+
+namespace GamejamCheese
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			AnsiConsole.Markup("[underline bold red]Hello[/] World!");
+		}
+	}
+}
