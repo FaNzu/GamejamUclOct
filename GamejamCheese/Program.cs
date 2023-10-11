2b13f188b8a0915887ee23f9ad3f4ecf17f7214c
OwlInABox <nicolaiskytte@hotmail.com>
2023-10-11 14:28:14 +0200

    progress on encountercontroller

--- GamejamCheese/Program.cs
+++ GamejamCheese/Program.cs
@@ -2,6 +2,8 @@
 using GamejamCheese.View;
 using Spectre.Console;
 using GamejamCheese.Models;
+using GamejamCheese.Controller;
+
 namespace GamejamCheese
 {
 	internal class Program
@@ -10,13 +12,14 @@ namespace GamejamCheese
 		{
 			EncounterManager.show();
 
-			//var image = new CanvasImage("../../../View/Pictures/spaceship1.png");
-			//image.MaxWidth = 25;
-			//AnsiConsole.Write(image);
-			//AnsiConsole.Markup("[underline bold red]Hello[/] World!");
-			//EncounterManager encounterManager = new EncounterManager();
-			//encounterManager.Encounter(EncounterType.Alien);
-			SectionView section = new SectionView();
+            //var image = new CanvasImage("../../../View/Pictures/spaceship1.png");
+            //image.MaxWidth = 25;
+            //AnsiConsole.Write(image);
+            //AnsiConsole.Markup("[underline bold red]Hello[/] World!");
+
+            EncounterController encounterController = new EncounterController();
+            encounterController.Encounter(EncounterType.Alien);
+            SectionView section = new SectionView();
 			section.StartMenu();
 			Console.ReadLine();
 		}
