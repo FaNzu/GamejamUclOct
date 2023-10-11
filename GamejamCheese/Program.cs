06f140ea2488ca2682d58a768f183a848d74e207
HasseG <hassegregers@gmail.com>
2023-10-11 15:47:03 +0200

    Changed SectionView to use Player instead of magic numbers

--- GamejamCheese/Program.cs
+++ GamejamCheese/Program.cs
@@ -9,22 +9,12 @@ namespace GamejamCheese
 	{
 		static void Main(string[] args)
 		{
-			EncounterManager encounterManager = new EncounterManager();
-			EncounterManager.Show(EncounterType.Alien);
+			
 
 			//var image = new CanvasImage("../../../View/Pictures/spaceship1.png");
 			//image.MaxWidth = 25;
 			//AnsiConsole.Write(image);
 			//AnsiConsole.Markup("[underline bold red]Hello[/] World!");
-			//encounterManager.Encounter(EncounterType.Alien);
-			//Section section = new Section("Mars", "This planet has no form of life", "Dig a hole", null, Modifier.ZeroOxygen);
-			//SectionController.NewSection(section);
-			Console.ReadLine();
-
-            //var image = new CanvasImage("../../../View/Pictures/spaceship1.png");
-            //image.MaxWidth = 25;
-            //AnsiConsole.Write(image);
-            //AnsiConsole.Markup("[underline bold red]Hello[/] World!");
 
 			//For at teste section view
 			//Section section = new Section("Mars", "This planet has no form of life", "Dig a hole", null, Modifier.ZeroOxygen);
@@ -37,16 +27,12 @@ namespace GamejamCheese
 			//}
 			//InventoryController.Show();
 
-            EncounterController encounterController = new EncounterController();
-            encounterController.Encounter(EncounterType.Alien);
+			//EncounterController encounterController = new EncounterController();
+			//encounterController.Encounter(EncounterType.Alien);
 			Section section = new Section("Mars", "This planet has no form of life", "Dig a hole", null, Modifier.ZeroOxygen);
 			SectionController.NewSection(section);
 			Console.ReadLine();
-   //         EncounterController encounterController = new EncounterController();
-   //         encounterController.Encounter(EncounterType.Alien);
-			//Section section = new Section("Mars", "This planet has no form of life", "Dig a hole", null, Modifier.ZeroOxygen);
-			//SectionController.NewSection(section);
-			//Console.ReadLine();
+			
 		}
 	}
 }
