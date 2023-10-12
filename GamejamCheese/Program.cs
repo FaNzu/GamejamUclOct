9e6ea81352e0bf0d3f79c3b44ef7ce42fa84d907
Rasmus Lund <Rasmuslund67@gmail.com>
2023-10-12 13:34:18 +0200

    Added Sections in DataInitilizer

--- GamejamCheese/Program.cs
+++ GamejamCheese/Program.cs
@@ -8,41 +8,7 @@ namespace GamejamCheese
 	{
 		static void Main(string[] args)
 		{
-			//New Connections
-			Connection LandingZoneToTheMartian = new Connection();
-			Connection TheMartianToDoctorZed = new Connection();
-
-			//New sections
-			Section MarsOrbit = new Section("Mars", "This planet has no form of life", "Scan planet for lifeforms", Modifier.LowGravity);
-			Section MarsLandingZone = new Section("Mars landing zone", "This landing zone does not seem safe....", "Refuel ship", Modifier.HighGravity);
-			Section TheMartian = new Section("The Martian", "This planet has no form of life", "Ask for roumers", Modifier.HighGravity);
-			Section DoctorZed = new Section("Doctor Zed", "This planet has no form of life", "Ask Doctor Zed to patch you op", Modifier.HighGravity);
-
-			Connection MarsOrbitToLandingZone = new Connection();
-
-
-			//Connecting sections
-			MarsOrbitToLandingZone.NextSection = MarsLandingZone;
-			MarsOrbitToLandingZone.LastSection = MarsOrbit;
-			MarsOrbitToLandingZone.IsVisible = true;
-
-			MarsOrbit.Connections.Add(MarsOrbitToLandingZone);
-
-			LandingZoneToTheMartian.NextSection = TheMartian;
-			LandingZoneToTheMartian.LastSection = MarsLandingZone;
-			LandingZoneToTheMartian.IsVisible = true;
-
-			MarsLandingZone.Connections.Add(LandingZoneToTheMartian);
-
-			TheMartianToDoctorZed.NextSection = DoctorZed;
-			TheMartianToDoctorZed.LastSection = TheMartian;
-			TheMartianToDoctorZed.IsVisible = true;
-
-			TheMartian.Connections.Add(TheMartianToDoctorZed);
-
-			//Starting the game at mars orbit
-			SectionController.StartSection(MarsOrbit);
-			Console.ReadLine();
+            
 		}
 	}
 }
